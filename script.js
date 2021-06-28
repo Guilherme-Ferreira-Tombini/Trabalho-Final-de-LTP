786258b8b5e6e530fec4114d42e833356660916a
GuilhermeTombin <>
2021-06-28 20:59:53 +0000

    Script.js modificado p

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ request.send();
 request.onload = function () {
   var response = request.response;
   
-  response.forEach(response => {
-    let titulo = response.articles.title;
-    let dataDePublicacao = response.articles.publishedAt;
-    let resumo = response.articles.description;
-    let autorDaNoticia = response.articles.author;
-    let imagem = response.imagem;
+  response.articles.forEach(response => {
+    let titulo = response.title;
+     let dataDePublicacao = response.publishedAt;
+    let resumo = response.description;
+    let autorDaNoticia = response.author;
+    let imagem = response.urlToImage;
     
-    let objeto = new NoticiaDestaque(imagem, title, publishedAt, description, author);
+  let objeto = new NoticiaDestaque(imagem, title, publishedAt, description, author);
 
-    let noticias = document.getElementById("noticias");
-    noticias.insertAdjacentHTML('afterbegin', '<div id="test">' + objeto.mostrarDestaque + '<div>');
+  let noticias = document.getElementById("noticias");
+  noticias.insertAdjacentHTML('afterbegin', '<div id="test">' + objeto.mostrarDestaque + '<div>');
   })
 }
